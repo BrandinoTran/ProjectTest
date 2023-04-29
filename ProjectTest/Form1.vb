5155c1b4c32bae7c5e256a02df8999c852e9ffe5
Brandon Tran <trannbranndonn@gmail.com>
2023-04-29 16:51:05 -0500

    Added an Exit button, option statements and name

--- ProjectTest/Form1.vb
+++ ProjectTest/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿'Brandon Tran
+Option Infer Off
+Option Strict On
+Option Explicit On
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
 End Class
